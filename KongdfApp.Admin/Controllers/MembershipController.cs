db759a113f9b4d1c3e12b8b266400e3b76a1ddb2
Kent <kongdongfang@shinetechchina.com>
2017-08-04 17:48:54 +0800

    Add Ajax HTML pages

---
+++ KongdfApp.Admin/Controllers/MembershipController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KongdfApp.Admin.Controllers
+{
+    public class MembershipController : Controller
+    {
+        // GET: Membership
+        public ActionResult Users()
+        {
+            return View();
+        }
+
+		public ActionResult Roles() {
+			return View();
+		}
+
+		public ActionResult Modules() {
+			return View();
+		}
+
+		public ActionResult Permissions() {
+			return View();
+		}
+	}
+}
